cee6ac2a2562aafc42114fe25dadafeaee2229d1
Leslie <clarkie637@gmail.com>
2021-02-08 12:02:53 +0000

    Port change again

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/profile/:id', profile.handleProfileGet(db))
 app.put('/image', image.handleImage(db))
 app.post('/imageUrl', image.handleApiCall)
 
-app.listen(process.env.PORT || 5000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on port ${process.env.PORT}`)
 });
